e589a28646d25026bbead5ea75c7480c7301ddf8
Ryotaro <ryotaro20250303@gmail.com>
2025-09-30 15:41:57 +0900

    Finish state

--- 05_state_and_event/src/060_state_object/start/Example.jsx
+++ 05_state_and_event/src/060_state_object/start/Example.jsx
@@ -6,19 +6,26 @@ import { useState } from "react";
 const Example = () => {
   const personObj = { name: "Tom", age: 18 };
   const [person, setPerson] = useState(personObj);
+  console.log({...person});
   return (
     <>
-    <h3>Name:{person.name}</h3>
-    <h3>Age:{person.age}</h3>
-    <input type="text"
-    onChange={(e) => setPerson(e.target.value)}>
-    </input>
-    <input type="text"
-    onChange={(e) => setPerson(e.target.value)}>
-    </input>
-    <button onClick={() => setPerson()}>リセット</button>
+      <h3>Name:{person.name}</h3>
+      <h3>Age:{person.age}</h3>
+      <input
+        type="text"
+        value={person.name}
+        onChange={(e) => setPerson({ ...person, name: e.target.value})}
+      ></input>
+      <input
+        type="text"
+        value={person.age}
+        onChange={(e) => setPerson({ name: person.name, age: e.target.value })}
+      ></input>
+      <div>
+        <button onClick={() => setPerson({ name:"", age:""})}>リセット</button>
+      </div>
     </>
-  )
+  );
 };
 
 export default Example;
--- 05_state_and_event/src/064_state_array/start/Example.jsx
+++ 05_state_and_event/src/064_state_array/start/Example.jsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
+
 const Example = () => {
   const numArray = [1, 2, 3, 4, 5];
+  const [nums, setNums] = useState(numArray);
+
+  const shuffle = () => {
+    const newNums = [ ...nums ]; //配列を新しく作り、同じ要素を格納
+    const value = newNums.pop();
+    newNums.unshift(value);
+    setNums(newNums);
+  }
 
   return (
     <>
-      
+      <h1>{nums}</h1>
+      <button onClick={shuffle}>shuffle</button>
     </>
   );
 };
