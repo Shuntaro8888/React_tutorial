afa435e608f34eefaafafabb8e3077b63f1cb9bc
Ryotaro <ryotaro20250303@gmail.com>
2025-10-02 22:47:28 +0900

    styling 1

--- 07_styling_component/src/010_inline_style/start/Example.jsx
+++ 07_styling_component/src/010_inline_style/start/Example.jsx
@@ -5,10 +5,24 @@ const Example = () => {
 
     const clickHandler = () => setIsSelected(prev => !prev);
 
+    const style = {
+        width:120,
+        height: 60,
+        display: "block",
+        fontWeight: "bold",
+        "border-radius": 9999, 
+        cursor: "pointer", //hoverしたときにポインターとなる
+        border: "none",
+        margin: "auto",
+        background: isSelected ? "pink" : ""
+    }
+    // ::before, ::after, :hover, :active 疑似要素、疑似セレクターが使えない
+    // @media (min-width: 600px) メディアクエリが使えない
+
     return (
         <>
-            <button onClick={clickHandler}>ボタン</button>
-            <div>{isSelected && "クリックされました。"}</div>
+            <button onClick={clickHandler} style= {style}>ボタン</button>
+            <div style={{ textAlign: "center"}}>{isSelected && "クリックされました。"}</div>
         </>
     )
 };
