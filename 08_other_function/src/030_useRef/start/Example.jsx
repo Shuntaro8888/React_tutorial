c0db3f88879b3712b11bac15b871b583a65b7753
Ryotaro <ryotaro20250303@gmail.com>
2025-10-06 21:55:59 +0900

    Dom1

--- 08_other_function/src/030_useRef/start/Example.jsx
+++ 08_other_function/src/030_useRef/start/Example.jsx
@@ -1,15 +1,75 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const Case1 = () => {
   const [value, setValue] = useState("");
+  const inputRef = useRef();
 
   return (
     <div>
       <h3>ユースケース1</h3>
-      <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
-      <button onClick>
+      <input
+        type="text"
+        ref={inputRef}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button onClick={() => inputRef.current.focus()}>
         インプット要素をフォーカスする
       </button>
+    </div>
+  );
+};
+
+const Case2 = () => {
+  const [playing, setPlaying] = useState();
+  const videoRef = useRef();
+  return (
+    <div>
+      <h3>ユースケース2</h3>
+      <video style={{ maxWidth: "100%" }} ref={videoRef}>
+        <source src="./sample.mp4"></source>
+      </video>
+      <button
+        onClick={() => {
+          if (playing) {
+            videoRef.current.pause();
+          } else {
+            videoRef.current.play();
+          }
+          setPlaying((prev) => !prev);
+        }}
+      >
+        {playing ? "stop" : "Play"}
+      </button>
+    </div>
+  );
+};
+
+const Case3 = () => {
+  const createTimeStamp = () => new Date().getTime();
+  const [state, setState] = useState(createTimeStamp());
+  const ref = useRef(createTimeStamp());
+
+  const updateState = () => {
+    setState(createTimeStamp())
+  }
+
+  const updateRef = () => {
+    ref.current = createTimeStamp();
+    console.log(ref.current);
+  }
+
+  return (
+    <div>
+      <h3>ユースケース3</h3>
+      <div>
+        state: {state}
+        <button onClick={updateState}>更新</button>
+      </div>
+      <div>
+        ref: {ref.current}
+        <button onClick={updateRef}>更新</button>
+      </div>
     </div>
   );
 };
@@ -18,6 +78,8 @@ const Example = () => {
   return (
     <>
       <Case1 />
+      <Case2 />
+      <Case3 />
     </>
   );
 };
